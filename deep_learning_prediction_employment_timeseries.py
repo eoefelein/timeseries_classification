2d72a5928c0a8650adbadd93202e377a10388439
Erin Oefelein <oefeleinerin@gmail.com>
2021-09-15 17:15:51 -0500

    commenting out keras model to isolate error

--- deep_learning_prediction_employment_timeseries.py
+++ deep_learning_prediction_employment_timeseries.py
@@ -9,19 +9,18 @@ import numpy as np
 from functools import reduce
 import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
-# https://towardsdatascience.com/a-gentle-invitation-to-interactive-visualization-with-dash-a200427ccce9
 
-from keras.layers import (
-    Conv1D,
-    Dense,
-    Dropout,
-    Input,
-    Concatenate,
-    GlobalMaxPooling1D,
-    MaxPooling1D,
-    Flatten,
-)
-from keras.models import Model, Sequential
+# from keras.layers import (
+#     Conv1D,
+#     Dense,
+#     Dropout,
+#     # Input,
+#     Concatenate,
+#     GlobalMaxPooling1D,
+#     MaxPooling1D,
+#     Flatten,
+# )
+# from keras.models import Model, Sequential
 
 # Step 1. Launch the application
 app = dash.Dash()
@@ -36,7 +35,6 @@ X_countyname = pd.read_csv(
 # ensure all timeseries are all of the same length(86 days)
 # check_len_timeseries = []
 dropdown_counties = []
-
 for county in X_countyname["countyname"].unique():
     subset = X_countyname[X_countyname["countyname"] == county]
     if (
@@ -87,15 +85,37 @@ merged_data["emp_incbelowmed"] = merged_data["emp_incbelowmed"].astype(float)
 df = merged_data.pivot(index="date", columns="countyname", values="emp_incbelowmed")
 
 # dropdown options
-features = dropdown_counties
-opts = [{'label' : i, 'value' : i} for i in features]
+opts = [{'label' : i, 'value' : i} for i in dropdown_counties]
 
+# Step 4. Create a Dash layout
+app.layout = html.Div([
+                # a header and a paragraph
+                html.Div([
+                    html.H1("Employment Predictions per U.S. County"),
+                    html.P("Using Deep Learning to predict COVID-19 Recovery")],
+                    style = {'padding' : '50px' ,'backgroundColor' : '#3aaab2'}),
+                # dropdown
+                html.P([
+                    html.Label("Choose a county"),
+                    dcc.Dropdown(id = 'opt', options = opts,
+                                value = opts[0])
+                        ], style = {'width': '400px',
+                                    'fontSize' : '20px',
+                                    'padding-left' : '100px',
+                                    'display': 'inline-block'}),
+                # adding a plot
+                dcc.Graph(id = 'plot')
+])
+print(dropdown_counties)
 # Step 5. Add callback functions
-@app.callback(Output('plot', 'figure'),[Input('opt', 'value')])
-def update_figure(input, X_countyname, y_countyname):
-    selected = input
+@app.callback(
+    Output('plot', 'figure'),
+    [Input('opt', 'value')]
+    )
+def update_figure(input):
+    # input = str(input)
     # take out selected
-    X_minus_selected = X_countyname[X_countyname["countyname"] != selected]
+    X_minus_selected = X_countyname[X_countyname["countyname"] != input]
 
     # ensure all timeseries are all of the same length(86 days)
     check_len_timeseries = []
@@ -119,7 +139,7 @@ def update_figure(input, X_countyname, y_countyname):
         .reshape(len(counties), 86, 3)
         .astype(np.float32)
     )  # sample, timesteps, features
-    y_minus_selected = y_countyname[y_countyname["countyname"] != selected]
+    y_minus_selected = y_countyname[y_countyname["countyname"] != input]
     # create y
     total_perc_change = []
 
@@ -135,12 +155,13 @@ def update_figure(input, X_countyname, y_countyname):
         X, y, test_size=0.2, random_state=0
     )
     # add selected back to test data
-    X_selected = X_countyname[X_countyname["countyname"] == selected][
+    X_selected = X_countyname[X_countyname["countyname"] == input][
         ["new_case_rate", "gps_away_from_home", "spend_all"]
     ].to_numpy()
+    X_selected.reshape(1, 86, 3)
     # add selected back to test
     X_test = np.vstack((X_test, X_selected.reshape(1, 86, 3)))
-    y_subset = y_countyname[y_countyname["countyname"] == selected]
+    y_subset = y_countyname[y_countyname["countyname"] == input]
     y_selected = reduce(lambda x, y: x + y + x * y, y_subset["emp_incbelowmed"], 1)
     y_selected = np.array(y_selected).astype(np.float32)
     y_test = np.append(y_test, y_selected)
@@ -151,54 +172,54 @@ def update_figure(input, X_countyname, y_countyname):
         1,
     )
 
-    def create_model(verbose=0, epochs=10):
-        model = Sequential()
-        # solves what is called the vanishing gradient problem whereby
-        # the neural network would not be able to feed back important gradient information
-        # from the output layer back to the input layer
-        model.add(
-            Conv1D(
-                filters=10,
-                kernel_size=1,
-                activation="relu",  # popular with regression neural nets
-                input_shape=(n_timesteps, n_features),
-            )
-        )
-        model.add(Conv1D(filters=10, kernel_size=1, activation="relu"))
-        model.add(Dropout(0.5))
-        # model.add(MaxPooling1D(pool_size=2))
-        model.add(Flatten())
-        model.add(Dense(50, activation="elu"))  # try activation='elu' ???
-        # Initializers define the way to set the initial random weights of Keras layers.
-        model.add(Dense(n_outputs, kernel_initializer="normal", activation="linear"))
-        return model
+    # def create_model(verbose=0, epochs=10):
+    #     model = Sequential()
+    #     # solves what is called the vanishing gradient problem whereby
+    #     # the neural network would not be able to feed back important gradient information
+    #     # from the output layer back to the input layer
+    #     model.add(
+    #         Conv1D(
+    #             filters=10,
+    #             kernel_size=1,
+    #             activation="relu",  # popular with regression neural nets
+    #             input_shape=(n_timesteps, n_features),
+    #         )
+    #     )
+    #     model.add(Conv1D(filters=10, kernel_size=1, activation="relu"))
+    #     model.add(Dropout(0.5))
+    #     # model.add(MaxPooling1D(pool_size=2))
+    #     model.add(Flatten())
+    #     model.add(Dense(50, activation="relu"))  # try activation='elu' ???
+    #     # Initializers define the way to set the initial random weights of Keras layers.
+    #     model.add(Dense(n_outputs, kernel_initializer="normal", activation="linear"))
+    #     return model
 
-    # create the model
-    model = create_model()
-    model.compile(
-        loss="mean_absolute_error", optimizer="adam", metrics=["mean_absolute_error"]
-    )
-    model.fit(X_train, y_train)
-    weights = model.get_weights()
-    ## create single item model
-    single_item_model = create_model()
-    single_item_model.set_weights(weights)
-    single_item_model.compile(
-        loss="mean_absolute_error", optimizer="adam", metrics=["mean_absolute_error"]
-    )
+    # # create the model
+    # model = create_model()
+    # model.compile(
+    #     loss="mean_absolute_error", optimizer="adam", metrics=["mean_absolute_error"]
+    # )
+    # model.fit(X_train, y_train)
+    # weights = model.get_weights()
+    # ## create single item model
+    # single_item_model = create_model()
+    # single_item_model.set_weights(weights)
+    # single_item_model.compile(
+    #     loss="mean_absolute_error", optimizer="adam", metrics=["mean_absolute_error"]
+    # )
 
-    prediction = single_item_model.predict(
-        np.array(X_test[-1], ndmin=3)
-    )  # pass counties.index('countyname')
-    print(prediction[0])
+    # prediction = single_item_model.predict(
+    #     np.array(X_test[-1], ndmin=3)
+    # )  # pass counties.index('countyname')
+    # print(prediction[0])
+    prediction = -.2784392
 
     # updating the plot
-    dates = df.index.append(pd.Series(pd.to_datetime("2021-10-24"))).reset_index(drop=True)
-    employment = df[input].append(pd.Series(prediction[0])).reset_index(drop=True)
+    dates = df.index.append(pd.Index([pd.to_datetime("2021-10-24")])).values
+    employment = df[input].append(pd.Series(prediction)).values
     fig = go.Figure([go.Bar(x=dates, y=employment)])
     # fig = px.bar(df, x=df.index, y=df[input])
     return fig
-
 # index = counties.index(input)
 # single_item_model.predict(np.array(X[index], ndmin=3)) # pass counties.index('countyname')
 
